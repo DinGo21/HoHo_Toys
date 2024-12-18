ce0626f9a09954e03a85b785599c0276bc9e36dc
rebecabernal <rebecabernalmesa@gmail.com>
2024-12-18 13:26:13 +0100

    feat: Api toy controller routes add

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ToyController;
 use App\Http\Controllers\Api\ChildController;
 
 Route::get('/',[ChildController::class, 'index'])->name('apiSantaHome');
@@ -9,3 +10,10 @@ Route::get('/santa/{id}',[ChildController::class, 'show'])->name('apiSantaShow')
 Route::delete('/santa/{id}',[ChildController::class, 'destroy'])->name('apiSantaDestroy');
 Route::post('/santa',[ChildController::class, 'store'])->name('apiSantaStore');
 Route::put('/santa/{id}',[ChildController::class, 'update'])->name('apiSantaUpdate');
+
+Route::get('/elf', [ToyController::class, 'index'])->name('apiElvesHome');
+Route::delete('/elf/{id}', [ToyController::class, 'destroy'])->name('apiElvesDestroy');
+Route::post('/elf', [ToyController::class, 'store'])->name('apiElvesStore');
+Route::put('/elf/{id}', [ToyController::class, 'update'])->name('apiElvesUpdate');
+Route::get('/elf/{id}',[ToyController::class, 'show'])->name('apiElvesShow');
+
