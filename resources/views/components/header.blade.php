2076d7e2fbfedcde7781e002da096c392d891aba
DinGo21 <diego.antoniosantamaria@gmail.com>
2024-12-19 13:16:27 +0100

    fix: Fixed header and Nav

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,13 +1,7 @@
 <header>
     <h1 class="headerTitle">
         (HoHo
-        <a href="{{ route('home') }}"> <img src="{{ config('images.xmasTree')}}" alt="Christmas Tree"></a>
+        <a href="{{ route('home') }}"><img src="{{ config('images.xmasTree')}}" alt="Christmas Tree" /></a>
         Toys)
     </h1>
-    <nav class="headerNav">
-        <div class="headerNavContainer">
-            <a href="{{route('santa')}}" id="childrenLink" class="headerNavLink">Children</a>
-            <a href="{{route('elf')}}" id="toysLink" class="headerNavLink">Toys</a>
-        </div>
-    </nav>
 </header>
